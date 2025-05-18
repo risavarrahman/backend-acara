a7bb997c006218f3aaa438afcc3b2b030031afd3
Risav Arrahman <risavbm@gmail.com>
2025-05-18 22:05:10 +0700

    Setup API Login, Regist, MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./routes/api";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+  try {
+    const result = await db();
+    console.log("database status: ", result);
 
-const PORT = 3000;
+    const app = express();
+    const PORT = 3000;
 
-app.use("/api", router);
+    app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on https://localhost:${PORT}`);
-});
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on https://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
